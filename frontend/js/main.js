34c12e3c1bf48f5ad9fd094aa593a8002df93f92
Danka Niedziałkowska <vulpecula@vp.pl>
2013-02-16 22:33:48 +0100

    Drawning working

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -4,19 +4,47 @@ function Game() {
     this.width = canvas.attr('width');
     this.height = canvas.attr('height');
 
+    this.gameObjects = [];
     this.playerShip = new SpaceShip(0.5, 0.5, 0);
-    this.thrust = [0, 0];
+    this.playerProjectile = new Projectile(0.7, 0.7);
+    this.playerAsteroid = new Asteroid(0.23, 0.23, 1);
+    this.gameObjects[0] = this.playerShip;
+    this.gameObjects[1] = this.playerProjectile;
+    this.gameObjects[2] = this.playerAsteroid;
 }
 
 Game.prototype.translateTo = function(sprite) {
     this.ctx.translate(sprite.x*this.width, sprite.y*this.height);
 };
 
-Game.prototype.playerThrust = function(x) {
-
-};
+Game.prototype.getState = function(gameStateJSON) {
+    var inf = JSON.parse(gameStateJSON);
+    this.gameObjects = [];
+    for (var i = 0; i < inf.length; ++i) {
+        if (inf[i].type === "spaceship") {
+            this.gameObjects[i] = new SpaceShip(inf[i].x, inf[i].y, inf[i].rot);
+        } else if (inf[i].type === "projectile") {
+            this.gameObjects[i] = new Projectile(inf[i].x, inf[i].y);
+        } else if (inf[i].type === "asteroid") {
+            this.gameObjects[i] = new Asteroid(inf[i].x, inf[i].y, inf[i].size);
+        }
+    }
+}
 
 Game.prototype.update = function() {
+    var tmp0 = new SpaceShip(0.4, 0.4, 0.2);
+    tmp0.type = "spaceship";
+    var tmp1 = new SpaceShip(0.2, 0.2, 0.3);
+    tmp1.type = "spaceship";
+    var tmp2 = new Projectile(0.3, 0.3);
+    tmp2.type = "projectile";
+    var tmp3 = new Asteroid(0.1, 0.1, 5);
+    tmp3.type = "asteroid";
+    var tmpObjects = [tmp0, tmp1, tmp2, tmp3];
+    gameStateJSON = JSON.stringify(tmpObjects);
+    
+    this.getState(gameStateJSON);
+    this.redraw()
 
 };
 
@@ -24,10 +52,12 @@ Game.prototype.redraw = function() {
     this.ctx.fillStyle = "black";
     this.ctx.fillRect(0, 0, this.width, this.height);
 
-    this.ctx.save();
-    this.translateTo(this.playerShip);
-    this.playerShip.draw(this.ctx);
-    this.ctx.restore();
+    for (var i = 0; i < this.gameObjects.length; ++i) {
+        this.ctx.save();
+        this.translateTo(this.gameObjects[i]);
+        this.gameObjects[i].draw(this.ctx);
+        this.ctx.restore();
+    }
 };
 
 function SpaceShip(x, y, rot) {
@@ -37,6 +67,7 @@ function SpaceShip(x, y, rot) {
 }
 
 SpaceShip.prototype.draw = function(ctx) {
+    console.log("SpaceShip.draw()");
     ctx.strokeStyle = "white";
     ctx.rotate(this.rot);
     ctx.beginPath();
@@ -55,9 +86,11 @@ function Asteroid(x, y, size) {
 }
 
 Asteroid.prototype.draw = function(ctx) {
-    ctx.strokeStyle = 'white';
+    console.log("Asteroid.draw()");
+    ctx.strokeStyle = "white";
     ctx.beginPath();
     ctx.arc(0, 0, this.size*10, this.size*10, Math.PI*2, true); 
+    ctx.fill();
     ctx.closePath();
     ctx.stroke();
 };
@@ -68,8 +101,9 @@ function Projectile(x, y) {
 }
 
 Projectile.prototype.draw = function(ctx) {
-    ctx.strokeStyle = 'white';
-    ctx.fillStyle = 'white';
+    console.log("Projectile.draw()");
+    ctx.strokeStyle = "white";
+    ctx.fillStyle = "white";
     ctx.beginPath();
     ctx.arc(0, 0, 3, 3, Math.PI*2, true); 
     ctx.closePath();
@@ -84,25 +118,24 @@ $(document).ready(
         game = new Game();
         game.redraw();
 
-        ws = new WebSocket("ws://localhost:9160");
-        ws.onopen = function(){
+        //ws = new WebSocket("ws://192.168.1.100:9160");
+        //ws.onopen = function(){
             /*Send a small message to the console once the connection is established */
-            console.log('Connection open!');
-            ws.send("asd");
-        };
+        //    console.log('Connection open!');
+        //    ws.send("asd");
+        //};
 
         $("body").keypress(
             function(e) {
                 switch(e.which) {
-                case 37: game.thrustLeft = true; break;
-                case 38: game.thrustUp = true; break;
-                case 39: game.thrustRight = true; break;
-                case 40: game.thrustDown = true; break;
-                default: return false;
+                case 37: game.thrustLeft = true; break; //left arrow
+                case 38: game.thrustUp = true; break; //up arrow 
+                case 39: game.thrustRight = true; break; //right arrow
+                default: return;
                 }
                 e.preventDefault();
                 return true;
             });
-        $.doTimeout('main_loop', 500, function () { game.update(); return true; });
+        $.doTimeout('main_loop', 2000, function () { game.update(); return true; });
     }
 );
